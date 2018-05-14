dfa87755771c2664cc96504021717f149d8b5c47
Nkosi Ndlovu <nkosindlovu87@gmail.com>
2018-05-14 19:53:09 +0100

    update diffArray test

--- diffArrays.test.js
+++ diffArrays.test.js
@@ -1,5 +1,5 @@
 const diffArrays = require('./diffArrays');
 
-test('Returns correct order for 4', () => {
-    expect(diffArrays.diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5, 883, 98])).toEqual([4, 883, 98]);
+test('Returns 883, 98', () => {
+  expect(diffArrays.diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5, 883, 98])).toEqual([4, 883, 98]);
 });
